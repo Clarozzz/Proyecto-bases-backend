7a793817972b37e994c7f283180fa709e9344aec
JosueSalomon <josueisacsalomonlanda@gmail.com>
2023-11-14 12:23:15 -0600

    b

--- projects/models.py
+++ projects/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class prueba(models.Model):
     nombre = models.CharField(max_length=50)
+
+class usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    correo = models.CharField(max_length=50)
+    contraseña = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=50)
